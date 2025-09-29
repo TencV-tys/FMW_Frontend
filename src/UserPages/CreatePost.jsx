6a91a07c5912bbf0982ca0547153401275839f12
Vincent <vincenttayros078@gmail.com>
2025-09-29 20:24:59 +0800

    Create Post Form Done

--- src/UserPages/CreatePost.jsx
+++ src/UserPages/CreatePost.jsx
@@ -1,7 +1,8 @@
 
 
 
-import UserNav from '../UserComponents/UserDashboardNav'
+import UserNav from '../UserComponents/UserDashboardNav';
+import './styles/CreatePost.css';
 export default function CreatePost(){
 
 
@@ -10,10 +11,10 @@ export default function CreatePost(){
         <div className="create-container">
             <UserNav/>
            <main className="create-content">
-             <h2>Create a Post</h2>
-              <div className='create-form-container'>
-                 
+              <div className='create-form-container'> 
+                
                 <form className='create-post-form'>
+                  <h2>Create a Post</h2>
                      <div className='create-input-group'>
                         <input type='text'
                                name='name'
@@ -21,43 +22,47 @@ export default function CreatePost(){
                                required
                                />
                      </div>
-                     <div className='create-input-group'>
+                  <div className='create-select-container'>
+                     <div className='create-select-group'>
                         <select name='type' >
                            <option value="lost">Lost</option>
                            <option value="found">Found</option>
                         </select>
                      </div>
-                      <div className='create-input-group'>
+                      <div className='create-select-group'>
                           <select name='category_id'>
                            <option value="">Select Category</option>
                           </select>
                      </div>
-                     <div className='create-input-group'>
+                     <div className='create-select-group'>
                           <select name='barangay_id'>
                            <option value="">Select Barangay</option>
                           </select>
                      </div>
-                     
+                  </div>
                      <div  className='create-input-group'>
                         <input type='text'
                                name='color'
                                placeholder='Color (optional)'/>
                      </div>
-                     <div className='create-input-group'>
-                        <textarea name='descripion'
-                                  placeholder='Additional details'></textarea>
+                     <div className='create-textarea-group'>
+                        <textarea name='description'
+                                  rows={4}
+                                  placeholder='Additional details'>
+                                  </textarea>
                      </div>
-                      <div className='create-input-group'>
+                      <div className='create-textarea-group'>
                         <textarea name='contact_info'
+                                  rows={4}
                                   placeholder='How can people reach you? (e.g.,Call me:09877666677, Email: example@example.com)'
                                   ></textarea>
                      </div>
-                     <div className='create-input-group'>
+                     <div className='image-uploader-container'>
                         <input type='file'
                                name='image'
                                accept='image/*'/>
                      </div>
-                     <button type='submit'>Submit Post</button>
+                     <button className='submit-post-btn' type='submit'>Submit Post</button>
                 </form>
 
               </div>
