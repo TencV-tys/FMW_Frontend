929a5e1fa27558ef01e8de4404f7e1145ef35a68
Vincent <vincenttayros078@gmail.com>
2025-09-30 21:14:17 +0800

    Latest

--- src/UserPages/MyPosts.jsx
+++ src/UserPages/MyPosts.jsx
@@ -1,14 +1,37 @@
-
+import {Link} from 'react-router-dom'
 import UserNav from '../UserComponents/UserDashboardNav'
-
+import Logo1 from '../assets/Logo.jpg'
+import './styles/MyPosts.css'
 export default function MyPosts(){
 
 
 return(
-    <div className="mypost-container">
+    <div className="myposts-container">
         <UserNav/>
-        <main className="mypost-content">
-       <p>MyPosts</p>              
+        <main className="myposts-content">
+       <h2>MyPosts</h2>
+        <div className='myposts-list'>
+
+            <div className='mypost-cards'>
+                <div className='mypost-details-container'>
+                <div className='mypost-details'> 
+                <h3>post.title</h3>
+              <p>post.content</p>
+              <small>Category: post.category</small>
+              <small>Contact: post.contact_info</small>
+               </div>
+               <div className='mypost-image'>
+              <img src={Logo1} alt="image" />
+               </div>
+               </div>
+              <div className='mypost-actions'>
+                      <Link to=''>Edit</Link>
+                      <button>Delete</button>
+              </div>
+                
+            </div>
+           
+        </div>
         </main>
 
     </div>
