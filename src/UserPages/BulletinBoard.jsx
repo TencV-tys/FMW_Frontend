ff68e3e078d96922b7f0d7053287cb01b0df043c
Vincent <vincenttayros078@gmail.com>
2025-10-01 14:34:27 +0800

    Bulletin Board

--- src/UserPages/BulletinBoard.jsx
+++ src/UserPages/BulletinBoard.jsx
@@ -1,7 +1,8 @@
 
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import UserNav from '../UserComponents/UserDashboardNav.jsx';
-
+import './styles/BulletinBoard.css'
 export default function BulletinBoard(){
 
 
@@ -9,7 +10,73 @@ return(
     <div className="bulletin-page-container">
        <UserNav/>
       <main className="bulletin-container">
-      Bulletin Board
+
+        <div className='bulletin-board-container-darkbrown'>
+             
+             <div className='bulletin-board-container-lightbrown'>
+                  
+                  <div className='bulletin-board-content'>
+                   <div className='bulletin-board-title'>
+                        <h1>Lost & Found</h1>
+                   </div>
+                   <div className='search-input-container'>
+                      <div className='search-bar'>
+                        <input className='search-bar-input' type='text'
+                                  placeholder='Search...'  />
+                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                       </div>
+                   </div>
+                   <div className='lost-found-container'>
+                       <div className='lost-found-cards'>
+                            <span className='pin'></span>
+                            <h2>Lost Animals</h2>
+                            <div className='lost-found-contents'>
+                            <img src=''alt='animals'></img>
+                            <div className='lost-found-details'>
+                                 <h4>Lost Dog</h4>
+                                 <p>Black labrador last seen hear central park</p>
+                            </div>
+                            </div>
+                       </div>
+                       <div className='lost-found-cards'>
+                            <span className='pin'></span>
+                            <h2>Lost Animals</h2>
+                            <div className='lost-found-contents'>
+                            <img src=''alt='animals'></img>
+                            <div className='lost-found-details'>
+                                 <h4>Lost Dog</h4>
+                                 <p>Black labrador last seen hear central park</p>
+                            </div>
+                            </div>
+                       </div>
+                       <div className='lost-found-cards'>
+                            <span className='pin'></span>
+                            <h2>Lost Animals</h2>
+                            <div className='lost-found-contents'>
+                            <img src=''alt='animals'></img>
+                            <div className='lost-found-details'>
+                                 <h4>Lost Dog</h4>
+                                 <p>Black labrador last seen hear central park</p>
+                            </div>
+                            </div>
+                       </div>
+                       <div className='lost-found-cards'>
+                            <span className='pin'></span>
+                            <h2>Lost Animals</h2>
+                            <div className='lost-found-contents'>
+                            <img src=''alt='animals'></img>
+                            <div className='lost-found-details'>
+                                 <h4>Lost Dog</h4>
+                                 <p>Black labrador last seen hear central park</p>
+                            </div>
+                            </div>
+                       </div>
+                   </div>
+
+                   </div>
+             </div>
+        </div>
+
       </main>
 
     </div>
