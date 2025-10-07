4f09643e08318fcfd2171fd72d60678d48e49d5f
Vincent <vincenttayros078@gmail.com>
2025-10-07 20:20:31 +0800

    ClientV2 API implementation

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,37 +1,111 @@
 import NavAuth from "../components/NavAuth";
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import {useState} from 'react';
 import './styles/Registration.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignIn } from "@fortawesome/free-solid-svg-icons";
 export default function Registration(){
+   
+   const [formData, setFormData] = useState({
+    first_name:"",
+    last_name:"",
+    email:"",
+    gender:"",
+    password:"",
+    password_confirmation:""
+  });
+   const [ message, setMessage ] = useState('');
+   const nav = useNavigate();
+   const handleChange = (e) => {
+    setFormData({...formData,[e.target.name]: e.target.value });
+   }
+
+   const handleSubmit = async (e)=> {
+     e.preventDefault();
+     
+     try{
+      const response = await fetch("http://localhost:8000/api/register",{
+        method:"POST",
+        headers:{ "Content-Type": "application/json"},
+        body:JSON.stringify(formData)
+      });
+      
+      const data = await response.json();
+
+      if(response.ok){
+        setMessage("Registered successfully");
+        setFormData({
+          first_name:"",
+          last_name:"",
+          email:"",
+          gender:"",
+          password:"",
+          password_confirmation:""
+        });
+      }else{
+        setMessage(`${data.message || "Registration failed"}`);
+        console.log(`Failed registartion through:${data.message}`);
+      }
+
+     }catch(error){
+         setMessage("Network error:"+error.message);
+         console.log(`possible errors:${error.message}`);
+     }
+
+   }
+
+
 
   return(
     <>
     <NavAuth disabled="Hide"/>
     <div className="register-form-container">
      
-        <form className="register-container"> 
+        <form className="register-container" onSubmit={handleSubmit}> 
           <div className="register-form-title">
             <h2 className="create">Create Account</h2>
             </div>  
           <div className="input-group">
             <div className="input-first">
             <input type="text"
-            className="inputs"
-                   placeholder="First Name"/>
+                   className="inputs"
+                   placeholder="First Name"
+                   name="first_name"
+                   value={formData.first_name}
+                   onChange={handleChange}
+                   
+                   />
                    </div>
                    <div className="input-last">
                    <input type="text"
                    className="inputs"
-                   placeholder="Last Name"/>
+                   placeholder="Last Name"
+                   name="last_name"
+                   value={formData.last_name}
+                   onChange={handleChange}
+                  
+                   />
                  </div>            
           </div>    
           <div className="input-group">
-            <input className="inputs" type="email"
-                   placeholder="Email"/>
+            <input className="inputs" 
+                   type="email"
+                   placeholder="Email"
+                   name="email"
+                   value={formData.email}
+                   onChange={handleChange}
+                  
+                   />
           </div>
            <div className="input-group">
-           <select name="gender" class="gender-select-container">
+           <select 
+           name="gender" 
+           className="gender-select-container"
+           
+           value={formData.gender}
+           onChange={handleChange}
+          
+           >
             <option value="">Select gender</option>
            <option value="male">Male</option>
             <option value="female">Female</option>
@@ -40,12 +114,24 @@ export default function Registration(){
 
           </div>
           <div className="input-group">
-            <input className="inputs" type="password"
-                   placeholder="Password"/>
+            <input className="inputs" 
+                   type="password"
+                   placeholder="Password"
+                   name="password"
+                   value={formData.password}
+                   onChange={handleChange}
+                   
+                   />
           </div>
           <div className="input-group">
-            <input className="inputs" type="password"
-                   placeholder="Confirm password"/>
+            <input className="inputs" 
+                   type="password"
+                   placeholder="Confirm password"
+                   name="password_confirmation"
+                   value={formData.password_confirmation}
+                   onChange={handleChange}
+
+                   />
           </div>
           <div className="sign-in-btn">
             <button type="submit">Sign in</button>
@@ -55,6 +141,9 @@ export default function Registration(){
           <Link to="/login" className="Login-link">Login</Link>
           </div>
        </form>
+       {
+        message && <p>{message}</p>
+       }
     </div>
    </>
   )
