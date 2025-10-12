669a31a10a9472f43c6ae42bf33b2692e5b3f90e
Vincent <vincenttayros078@gmail.com>
2025-10-12 21:07:54 +0800

    added login btnin Landing page

--- src/components/NavAuth.jsx
+++ src/components/NavAuth.jsx
@@ -1,7 +1,10 @@
 import './styles/NavAuth.css';
 import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faSignIn} from '@fortawesome/free-solid-svg-icons';
+import {
+  faSignIn,
+  faDoorOpen
+} from '@fortawesome/free-solid-svg-icons';
 import Logo from '../assets/Logo2.jpg';
 export default function NavAuth({disabled}){
 
@@ -13,10 +16,14 @@ export default function NavAuth({disabled}){
             <img className='Logo-pic' src={Logo}/>
             FindMyWay</Link>      
             </div>
-            <div className={disabled}>
+            <div className={`landing-btn ${disabled}`}>
                 <Link to="/registration" className='nav-link'>
                Sign up
               <FontAwesomeIcon icon={faSignIn}/>
+              </Link>
+               <Link to="/login" className='nav-link'>
+               Login
+              <FontAwesomeIcon icon={faDoorOpen}/>
               </Link>
             </div>   
         </nav>
